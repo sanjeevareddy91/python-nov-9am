a50c18d321c9dbdf226a8c8217ac94524a509ce1
sanjeevareddy91 <gsanjeevreddy91@gmail.com>
2022-11-24 09:40:21 +0530

    added varibales_cls.py file

---
+++ varibales_cls.py
@@ -0,0 +1,36 @@
+# Variable : memory allocation of a value inside the system.
+
+a="Python"
+
+b="Python"
+
+# id - will return the memory allocation of the value.
+
+# print(id(a))
+
+# print(id(b))
+
+# # NOte: Any Python Line starting with #(Hash) will consider as commenting and will not be executed by Python.
+
+# a1 = 54
+# b4 = 65
+# print(a1)
+# print(b4)
+
+# 1a = 32
+# print(1a)
+
+# Rules for Declaring the varibales.
+    # 1) A variable can be the combination of anything(alphebets or number or special characters)
+        # EX: a1=87, A4=65, aA1=75, zW7=54, a1A=54
+    # 2) Any varibale should not be started with number.
+        # Ex : 1Z=54(Error)
+    # 3) No Special symbols cna used under varibales declaration except _(underscore).
+        # EX: A_1 = "Python", _A = "Data"
+    # 4) Single underscore can also be taken as varibale..
+        # Ex: _ = "Django"
+    # 5) Keywords cannot be taken as variables.
+
+class = 65
+
+print(class)
